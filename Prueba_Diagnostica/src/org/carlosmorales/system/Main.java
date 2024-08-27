820adf3685dec3a85fa6a18d7caa9a3bbeef69f1
Fredy <alexander.garcia.sicajau@gmail.com>
2024-08-27 11:42:41 -0600

    feat(Main)Se modifico el main

--- Prueba_Diagnostica/src/org/carlosmorales/system/Main.java
+++ Prueba_Diagnostica/src/org/carlosmorales/system/Main.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package org.carlosmorales.system;
 
-/**
- *
- * @author Usuario
- */
-public class Main {
+import org.fredygarcia.views.ViewGeneral;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        ViewGeneral vistaGeneral = new ViewGeneral();
+        vistaGeneral.mostrarMenuPrincipal();
     }
-    
 }
